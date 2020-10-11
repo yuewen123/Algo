45dbb3d86138e060395ea3bb5d3918224a10e1e9
yuewen123 <33101162+yuewen123@users.noreply.github.com>
2020-10-11 15:10:12 +0800

    Update hello.c
    hello word

--- hello.c
+++ hello.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <time.h>
-clock_t time_start,stop;
 int main()
 {
-	time_start = clock();
 	printf("Hello World!");
-	stop = clock();
-	printf("\n %f \n",(double)(time_start-stop));
 	return 0;
 	
 }
